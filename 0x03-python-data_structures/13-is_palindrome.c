601b2b5d6c23fc1327fefa5eed950399dd9cb006
Davis-sang <davissang08@gmail.com>
2022-08-02 13:21:17 +0300

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,30 @@
-
+#include "lists.h"
+#include<string>
+/*
+ * is_palindrome - checks a list if its a palindrome
+ * @head: first node
+ * Return: 0 if not a palindrome, 1 if it's a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (is_palind(head, *head));
+}
+/**
+ * is_palind - checks if palindrome
+ * @head: 1st node
+ * @end: tail node
+ * Return: 0 if not a palindrome, 1 if it's a paindrome
+ */
+int is_palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1)
+	if (is_palind(head, end->next) && (*head)->n == end ->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
